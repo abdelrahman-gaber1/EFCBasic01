3dc04740c07d9f083058801b2f41b5803e8d3c77
abdelrahman-gaber1 <abdogaber487@gmail.com>
2024-08-15 23:09:49 +0300

    Version1

---
+++ EFCBasic01/Course.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCBasic01
+{
+    internal class Course
+    {
+        public int Id { get; set; }
+
+        public DateTime Duration { get; set; }
+
+        public string Name { get; set; }    
+
+        public string Description { get; set; }
+
+        public int Top_Id { get; set; } 
+
+
+    }
+}
---
+++ EFCBasic01/Course_Ins.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCBasic01
+{
+    internal class Course_Ins
+    {
+        public int Inst_Id { get; set; }
+
+        public int Course_ID { get; set; }
+
+        public double Evaluate { get; set; }
+    }
+}
